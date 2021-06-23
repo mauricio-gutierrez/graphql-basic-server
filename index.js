9f7e3409b9e8f2d4f3cad8feb780107c310690a3
Mauricio Gutierrez <m@mgutierrez.co>
2021-06-23 11:49:14 -0300

    basic query implementation

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const { graphql, buildSchema } = require('graphql')
+
+// Schema definition
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+const resolvers = {
+    hello: () => {
+        return "World!";
+    }
+}
+
+// Run hello query
+graphql(schema, '{ hello }', resolvers).then((response) => {
+    const { data } = response;
+    console.log(data);
+});
